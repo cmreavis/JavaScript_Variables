4147773cf4671da51bf20146e4f6883bb9fd82ec
cmreavis97 <cmreavis97@gmail.com>
2022-10-17 14:43:50 -0400

    Exercises 1-5

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "";
+let lastName;
+var age;
 // Exercise 2
-
+firstName = "Colin";
+lastName = "Reavis";
+age = 25;
+console.log(firstName);
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 2005;
+let isCaseSensitive = true;
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hadRead = true;
